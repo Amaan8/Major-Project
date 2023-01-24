81feab906e450ba39e258633edafe97b444a9611
Amaan <amaanmas1@gmail.com>
2023-01-24 18:02:24 +0530

    Created Cart component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
+import {useState} from 'react';
 import {Container, Nav, Navbar, Button} from 'react-bootstrap';
+import Cart from './Cart';
 
 const Header = () => {
+  const [cartState, setCartState] = useState(false);
+
+  const showCart = () => {
+    setCartState(true);
+  }
+
+  const closeCart = () => {
+    setCartState(false);
+  }
+
   return (
     <>
-        <Navbar bg="dark" expand="lg" variant="dark">
-            <Container>
+        <Navbar bg="dark" expand="sm" variant="dark">
+            <Container fluid className='ms-2'>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mx-auto fs-5">
@@ -13,9 +25,10 @@ const Header = () => {
                     <Nav.Link href="#" className='ms-5'>About</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
-                <Button variant="info">Cart</Button>
+                <Button variant="info" onClick={showCart}>Cart</Button>
             </Container>
         </Navbar>
+        {cartState && <Cart onClose={closeCart} />}
         <div className='text-center text-white bg-secondary'>
             <h1 className='p-5 fw-bolder'>The Generics</h1>
         </div>
